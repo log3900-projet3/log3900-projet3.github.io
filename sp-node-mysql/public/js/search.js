496f7857ee595320327fa96729af41a76f3b21c3
“Vanou“ <vane.djerjian@polymtl.ca>
2016-04-04 22:24:43 -0400

    connection pool, pour fermer les connections

--- sp-node-mysql/public/js/search.js
+++ sp-node-mysql/public/js/search.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
-	//elements du button profils joueurs
+	//elements du button recherche
 	$("#rechercheJoueur").hide();
 	$("#profilsJoueur").hide();
+	$("#profilsPrive").hide();
 	
-	//elements du button de reglements
+	//elements du button profils de joueurs
 	$("#txtListe").hide();
 	$("#listeUsername").hide();
 	document.getElementById("listeUsername").innerHTML = "";
@@ -13,13 +14,15 @@ $(document).ready(function(){
 	$("#pageCredits").hide();		
 	
 	
-	$("#buttonJoueur").click(function(){
+	//Premier bouton Recherche
+	$("#buttonRecherche").click(function(){
 	
-		//elements du button profils joueurs
+		//elements du button recherche
 		$("#rechercheJoueur").show();
 		$("#profilsJoueur").hide();
+		$("#profilsPrive").hide();
 		
-		//elements du button de reglements
+		//elements du button profils de joueurs
 		$("#txtListe").hide();
 		$("#listeUsername").hide();
 		document.getElementById("listeUsername").innerHTML = "";
@@ -29,15 +32,52 @@ $(document).ready(function(){
 		
 	});
 	
+	//Deuxième bouton Profils de joueur
 	$("#buttonListe").click(function(){
 	
-		//elements du button de reglements
+		//elements du button profils de joueurs
+		//A VOIR
+		document.getElementById("listeUsername").innerHTML = "";
+		
+		var buttonArray = [];
+		$.get('http://localhost:5050/liste').done(function(dataName){
+			
+			//afficher tous les noms reçus (clickable)
+			for(var i = 0; i < dataName.length; i++){
+				
+				var para = document.createElement("P");        				// Create a <p> element
+				var t = document.createTextNode(dataName[i]);				// Create a text node for p
+				var viewNext = document.createElement("BUTTON");       		// Create a <button> element
+				var id = dataName[i];
+				viewNext.setAttribute("id", id);
+				var next = document.createTextNode("	>>");				// Create a text node for button
+				viewNext.appendChild(next);                                	// Append the text to <button>
+				
+				viewNext.addEventListener("click", function(e){
+				console.log(e.target.id + " est clické");
+				search(e.target.id);
+				});
+				
+				console.log(viewNext);
+				
+				var style = document.createElement("STYLE");
+    			var styleTXT = document.createTextNode("body {align: center; font: 16px times; line-height: 14.0px; color: #E77471;}");
+    			style.appendChild(styleTXT);
+				
+				para.appendChild(t);                                	// Append the text to <p>
+				para.appendChild(viewNext);                                	// Append the button to <p>
+				para.appendChild(style); 
+    			document.getElementById("listeUsername").appendChild(para);
+			}
+		});
+		
 		$("#txtListe").show();
 		$("#listeUsername").show();
 		
-		//elements du button profils joueurs
+		//elements du button recherche
 		$("#rechercheJoueur").hide();
 		$("#profilsJoueur").hide();
+		$("#profilsPrive").hide();
 		
 		//elements du button credits
 		$("#pageCredits").hide();		
@@ -49,11 +89,12 @@ $(document).ready(function(){
 		//elements du button credits
 		$("#pageCredits").show();		
 			
-		//elements du button profils joueurs
+		//elements du button recherche
 		$("#rechercheJoueur").hide();
 		$("#profilsJoueur").hide();
+		$("#profilsPrive").hide();
 		
-		//elements du button de reglements
+		//elements du button profils de joueurs
 		$("#txtListe").hide();
 		$("#listeUsername").hide();
 		document.getElementById("listeUsername").innerHTML = "";
@@ -65,18 +106,23 @@ $(document).ready(function(){
 		var nomChercher = document.getElementById("input").value ;
 		if (nomChercher != ' '){
 			$.get('http://localhost:5050/recherche?user='+ nomChercher).done(function(data){
-				
-				document.getElementById("varNom").innerHTML = data[1] ;	
-				document.getElementById("nbJouer").innerHTML = data[2] ;	
-				document.getElementById("nbGagner").innerHTML = data[3] ;	
-				document.getElementById("v-d").innerHTML = data[4] ;	
-				document.getElementById("nbBalles").innerHTML = data[5] ;	
-				document.getElementById("nbPoints").innerHTML = data[6] ;	
-				document.getElementById("succes").innerHTML = data[7] ;
-			
-			});
+				if(data[8] == 1){
+					document.getElementById("varNom").innerHTML = data[1] ;	
+					document.getElementById("nbJouer").innerHTML = data[2] ;	
+					document.getElementById("nbGagner").innerHTML = data[3] ;	
+					document.getElementById("v-d").innerHTML = data[4] ;	
+					document.getElementById("nbBalles").innerHTML = data[5] ;	
+					document.getElementById("nbPoints").innerHTML = data[6] ;	
+					document.getElementById("succes").innerHTML = data[7] ;
 						
-			$("#profilsJoueur").show();
+					$("#profilsJoueur").show();
+					$("#profilsPrive").hide();
+				}
+				else if(data[8] == 0){
+					$("#profilsJoueur").hide();
+					$("#profilsPrive").show();
+				}
+			});
 		}
 	});
 	
@@ -94,72 +140,18 @@ $(document).ready(function(){
 			
 		});
 		
-		//elements du button profils joueurs
+		//elements du button recherche
 		$("#rechercheJoueur").hide();
+		$("#profilsPrive").hide();
 		$("#profilsJoueur").show();
 		
-		//elements du button de reglements
+		//elements du button de profils de joueurs
 		$("#txtListe").hide();
 		$("#listeUsername").show();
-		//document.getElementById("listeUsername").innerHTML = "";
+		document.getElementById("listeUsername").innerHTML = "";
 
 		//elements du button credits
 		$("#pageCredits").hide();
 	}
-	
-	function makeRed(element, clr){
-		document.getElementById(element).style.color = clr;
-	}
-	
-	$('#buttonListe').click(function(){
-
-		var buttonArray = [];
-		$.get('http://localhost:5050/liste').done(function(dataName){
-			
-			//afficher tous les noms reçus (clickable)
-			for(var i = 0; i < dataName.length; i++){
-				
-				var para = document.createElement("P");        				// Create a <p> element
-				var t = document.createTextNode(dataName[i]);				// Create a text node for p
-				var viewNext = document.createElement("BUTTON");       		// Create a <button> element
-				var id = dataName[i];
-				viewNext.setAttribute("id", id);
-				var next = document.createTextNode("	>>");				// Create a text node for button
-				viewNext.appendChild(next);                                	// Append the text to <button>
-				
-				viewNext.addEventListener("click", function(e){
-				console.log(e.target.id + " est clické");
-				e.target.style.color = 'red';
-				search(e.target.id);
-				});
-				
-				console.log(viewNext);
-				
-				var style = document.createElement("STYLE");
-    			var styleTXT = document.createTextNode("body {align: center; font: 16px times; line-height: 14.0px; color: #E77471;}");
-    			style.appendChild(styleTXT);
-				
-				para.appendChild(t);                                	// Append the text to <p>
-				para.appendChild(viewNext);                                	// Append the button to <p>
-				para.appendChild(style); 
-    			document.getElementById("listeUsername").appendChild(para);
-			}
-			
-			/*for(var i = 0; i < dataName.length; i++){
-				var id = dataName[i] +"id";
-				console.log(id);
-				document.getElementById(id).addEventListener("click", function(){
-				console.log(id + "est clické");
-				document.getElementById(id).style.color = 'red';
-				
-				});*/
-				//document.getElementById(id).onclick = search(dataName[i]);
-
-			//}
-				
-			
-		});
-	});
-	
 });
 	
--- sp-node-mysql/server.js
+++ sp-node-mysql/server.js
@@ -2,158 +2,208 @@
 //------------------------------------------------------------
 // Database connection
 //------------------------------------------------------------
-console.log('Initializing database');
-var mysql = require('mysql');
+//function connection(){
+	console.log('Initializing database');
+	var mysql = require('mysql');
 
 
-var connection = mysql.createConnection({
-  			host: "us-cdbr-azure-central-a.cloudapp.net",
-  			user: "b0212399fa1965",
-  			password: "4c90e9db",
-  			database: "pinballgr8db"
-			});
+	var pool = mysql.createPool({
+				connectionLimit: 3,
+  				host: "us-cdbr-azure-central-a.cloudapp.net",
+  				user: "b0212399fa1965",
+  				password: "4c90e9db",
+  				database: "pinballgr8db"
+				});
 
-connection.connect(function(err) {
-	if (err) throw err
-	console.log('Database connection successful');
-});
-
-
+//	connection.connect(function(err) {
+//		if (err) throw err
+//		console.log('Database connection successful');
+//	});
 
 //------------------------------------------------------------
 // Page loading
 //------------------------------------------------------------
-console.log('Initializing server');
-var express = require('express');
-var path = require('path');
+	console.log('Initializing server');
+	var express = require('express');
+	var path = require('path');
 
+	var app = express();
+	app.use(express.static('public'));
 
-var app = express();
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
+	app.get('/', function (req, res) {
 	
-	res.sendFile(path.join(__dirname + '/index.html'));
+		res.sendFile(path.join(__dirname + '/index.html'));
 
-});
+	});
+//}
 
-// bouton Profils de joueurs est cliqué
+
+
+
+///////// bouton Profils de joueurs est cliqué/////////////
 app.get('/liste', function (req, res) {
-	console.log('bouton profils est cliqué, on affiche les noms');
+
+	console.log('PROFILS DE JOUEURS -> on les affiche tous');
 	var dataName = [];
 	var names = "";
-	//requête pour afficher tous les joueurs
-	connection.query(qstr1, function(err, rows, fields) {
-		if (err) throw err;
-		
-		for (var i = 0; i < rows.length; i++) {
-			//console.log('Row #', i, 'was returned with', rows[i].username);
-			
-			dataName.push(rows[i].username);
-    	}
-    	res.send(dataName);
+	
+	pool.getConnection(function(err, connection1) {
+		//requête pour afficher tous les joueurs
+		connection1.query(qstr1, function(err, rows, fields) {
+			if (err) throw err;
+			for (var i = 0; i < rows.length; i++) {
+				if(rows[i].permission == 1)
+					dataName.push(rows[i].username);
+    		}
+    		res.send(dataName);
+    		
+    		connection1.release();
+		});
 	});
 });
-
-// bouton chercher est cliqué
+///////////// LOUPE -> on affiche le profils trouvé////////////////
 app.get('/recherche', function (req, res) {
-	console.log('bouton recherche est cliqué, on affiche le profils trouvé');
+
+	console.log('LOUPE -> on affiche le profils trouvé');
 
 	//nom du joueur voulu
 	var nomChercher = req.query.user;
 	var data = 0;
 	var listeSucces = '';
 	
-	//requête pour chercher les succes du joueur voulu
-	connection.query(qstr2 + "'" + nomChercher + "'", function(err, rowsSucces, fields) {
-		if (err) throw err;
+	pool.getConnection(function(err, connection2) {
+		//requête pour chercher les succes du joueur voulu
+		connection2.query(qstr2 + "'" + nomChercher + "'", function(err, rowsSucces, fields) {
+			if (err) throw err;
+			var dataSucces = 0;
 		
-		var dataSucces = 0;
-		
-		if(rowsSucces[0] === undefined){
-        	console.log('user does not exist in database');
-        	//alert here TODO
-        }
-        else{
-    		dataSucces =[rowsSucces[0].username , rowsSucces[0].firstSkin, rowsSucces[0].loveIsInTheAir, rowsSucces[0].eyeOnTheTarget, rowsSucces[0].goldDigger,
-			rowsSucces[0].gottaCatchThemAll, rowsSucces[0].point1, rowsSucces[0].point2, rowsSucces[0].point3, rowsSucces[0].point4, rowsSucces[0].point5,
-			rowsSucces[0].argent1, rowsSucces[0].argent2, rowsSucces[0].argent3, rowsSucces[0].argent4, rowsSucces[0].argent5, rowsSucces[0].wins1, rowsSucces[0].wins2,
-			rowsSucces[0].wins3, rowsSucces[0].wins4, rowsSucces[0].wins5, rowsSucces[0].games1, rowsSucces[0].games2, rowsSucces[0].games3, rowsSucces[0].games4, rowsSucces[0].games5];
+			if(rowsSucces[0] === undefined){
+        		console.log('user does not exist in database');
+			}
+			else {
+				if(String(rowsSucces[0].username) == ''){
+        		console.log('user = (Juste un space)');
+  	    	 	 }
+   	    		else {
+    				dataSucces =[rowsSucces[0].username , rowsSucces[0].firstSkin, rowsSucces[0].loveIsInTheAir, rowsSucces[0].eyeOnTheTarget, rowsSucces[0].goldDigger,
+					rowsSucces[0].gottaCatchEmAll, rowsSucces[0].point1, rowsSucces[0].point2, rowsSucces[0].point3, rowsSucces[0].point4, rowsSucces[0].point5,
+					rowsSucces[0].argent1, rowsSucces[0].argent2, rowsSucces[0].argent3, rowsSucces[0].argent4, rowsSucces[0].argent5, rowsSucces[0].wins1, rowsSucces[0].wins2,
+					rowsSucces[0].wins3, rowsSucces[0].wins4, rowsSucces[0].wins5, rowsSucces[0].games1, rowsSucces[0].games2, rowsSucces[0].games3, rowsSucces[0].games4, rowsSucces[0].games5];
 
-			var fieldsSucces = ['username' , 'First skin', 'Love is in the air', 'Eye on the target', 'gold digger',
-			'Gotta catch them all', 'Point 1', 'Point 2', 'Point 3', 'Point 4', 'Point 5',
-			'Argent 1', 'Argent 2', 'Argent 3', 'Argent 4', 'Argent 5', 'Wins 1', 'Wins 2',
-			'Wins 3', 'Wins 4', 'Wins 5', 'Games 1', 'Games 2', 'Games 3', 'Games 4', 'Games 5'];
+		 			var fieldsSucces = ['username' , 'First skin', 'Love is in the air', 'Eye on the target', 'gold digger',
+					"Gotta catch 'em all", 'Point 1', 'Point 2', 'Point 3', 'Point 4', 'Point 5',
+					'Argent 1', 'Argent 2', 'Argent 3', 'Argent 4', 'Argent 5', 'Wins 1', 'Wins 2',
+					'Wins 3', 'Wins 4', 'Wins 5', 'Games 1', 'Games 2', 'Games 3', 'Games 4', 'Games 5'];
 			
-			for(var i = 1; i < dataSucces.length; i++){
-				if(dataSucces[i] == 1 && i == 1){
-					listeSucces = listeSucces +fieldsSucces[i];
-				}
-				if(dataSucces[i] == 1 && i != (1)){
-					listeSucces = listeSucces + ', ' + fieldsSucces[i];
+					var firstAchievement = true;
+					
+					for(var i = 1; i < dataSucces.length; i++){
+						if(dataSucces[i] == 1 && firstAchievement){
+							listeSucces = listeSucces +fieldsSucces[i];
+							firstAchievement = false;
+						}
+						if(dataSucces[i] == 1){
+							listeSucces = listeSucces + ', ' + fieldsSucces[i];
+						}
+					}
 				}
 			}
-		}
-		//requête pour chercher le joueur voulu
-		connection.query(qstr3 + "'" + nomChercher + "'", function(err, rows, fields) {
-			if (err) throw err;
-			if(rowsSucces[0] === undefined)
-        		console.log('user does not exist in database: ');
-        	else{
-				data = ['rows[0].avatar', rows[0].username, rows[0].nbJouer, rows[0].nbGagner, rows[0].ratioVictoire, rows[0].nbBallePerdue, rows[0].nbPoints, listeSucces];				
-				res.send(data);
-				}
+			pool.getConnection(function(err, connection3) {
+				//requête pour chercher le joueur voulu
+				connection3.query(qstr3 + "'" + nomChercher + "'", function(err, rows, fields) {
+					if (err) throw err;
+			
+					if(rows[0] === undefined){
+        			console.log('user does not exist in database');
+					}
+					else {
+						if(String(rows[0].username) == '')
+        					console.log('user = (Juste un space)');
+        				else {
+        					console.log(rows[0].permission);
+        					if(rows[0].permission == 1){
+								data = ['rows[0].avatar', rows[0].username, rows[0].nbJouer, rows[0].nbGagner, rows[0].ratioVictoire, rows[0].nbBallePerdue, rows[0].nbPoints, listeSucces, rows[0].permission];				
+								res.send(data);
+							}
+						}
+					}
+					connection3.release();
+				});
+			});
+			connection2.release();
 		});
 	});
 });
 
+
+///////////// >> , cliqué à partir de la liste ////////////////
 app.get('/fonctionSearch', function (req, res) {
-	console.log('bouton recherche est cliqué, on affiche le profils trouvé');
+
+	console.log('>> , cliqué à partir de la liste');
 
 	//nom du joueur voulu
 	var nomChercher = req.query.user;
 	var data = 0;
 	var listeSucces = '';
 	
-	//requête pour chercher les succes du joueur voulu
-	connection.query(qstr2 + "'" + nomChercher + "'", function(err, rowsSucces, fields) {
-		if (err) throw err;
+	pool.getConnection(function(err, connection4) {
+		//requête pour chercher les succes du joueur voulu
+		connection4.query(qstr2 + "'" + nomChercher + "'", function(err, rowsSucces, fields) {
+			if (err) throw err;
 		
-		var dataSucces = 0;
+			var dataSucces = 0;
 		
-		if(rowsSucces[0] === undefined){
-        	console.log('user does not exist in database');
-        	//alert here TODO
-        }
-        else{
-    		dataSucces =[rowsSucces[0].username , rowsSucces[0].firstSkin, rowsSucces[0].loveIsInTheAir, rowsSucces[0].eyeOnTheTarget, rowsSucces[0].goldDigger,
-			rowsSucces[0].gottaCatchThemAll, rowsSucces[0].point1, rowsSucces[0].point2, rowsSucces[0].point3, rowsSucces[0].point4, rowsSucces[0].point5,
-			rowsSucces[0].argent1, rowsSucces[0].argent2, rowsSucces[0].argent3, rowsSucces[0].argent4, rowsSucces[0].argent5, rowsSucces[0].wins1, rowsSucces[0].wins2,
-			rowsSucces[0].wins3, rowsSucces[0].wins4, rowsSucces[0].wins5, rowsSucces[0].games1, rowsSucces[0].games2, rowsSucces[0].games3, rowsSucces[0].games4, rowsSucces[0].games5];
+			if(rowsSucces[0] === undefined){
+        		console.log('user does not exist in database');
+			}
+			else {
+				if(String(rowsSucces[0].username) == ''){
+   		    	 	console.log('user does not exist in database');
+       	 		}
+        		else {
+    				dataSucces =[rowsSucces[0].username , rowsSucces[0].firstSkin, rowsSucces[0].loveIsInTheAir, rowsSucces[0].eyeOnTheTarget, rowsSucces[0].goldDigger,
+					rowsSucces[0].gottaCatchThemAll, rowsSucces[0].point1, rowsSucces[0].point2, rowsSucces[0].point3, rowsSucces[0].point4, rowsSucces[0].point5,
+					rowsSucces[0].argent1, rowsSucces[0].argent2, rowsSucces[0].argent3, rowsSucces[0].argent4, rowsSucces[0].argent5, rowsSucces[0].wins1, rowsSucces[0].wins2,
+					rowsSucces[0].wins3, rowsSucces[0].wins4, rowsSucces[0].wins5, rowsSucces[0].games1, rowsSucces[0].games2, rowsSucces[0].games3, rowsSucces[0].games4, rowsSucces[0].games5];
 
-			var fieldsSucces = ['username' , 'First skin', 'Love is in the air', 'Eye on the target', 'gold digger',
-			'Gotta catch them all', 'Point 1', 'Point 2', 'Point 3', 'Point 4', 'Point 5',
-			'Argent 1', 'Argent 2', 'Argent 3', 'Argent 4', 'Argent 5', 'Wins 1', 'Wins 2',
-			'Wins 3', 'Wins 4', 'Wins 5', 'Games 1', 'Games 2', 'Games 3', 'Games 4', 'Games 5'];
-			
-			for(var i = 1; i < dataSucces.length; i++){
-				if(dataSucces[i] == 1 && i == 1){
-					listeSucces = listeSucces +fieldsSucces[i];
-				}
-				if(dataSucces[i] == 1 && i != (1)){
-					listeSucces = listeSucces + ', ' + fieldsSucces[i];
+					var fieldsSucces = ['username' , 'First skin', 'Love is in the air', 'Eye on the target', 'gold digger',
+					'Gotta catch them all', 'Point 1', 'Point 2', 'Point 3', 'Point 4', 'Point 5',
+					'Argent 1', 'Argent 2', 'Argent 3', 'Argent 4', 'Argent 5', 'Wins 1', 'Wins 2',
+					'Wins 3', 'Wins 4', 'Wins 5', 'Games 1', 'Games 2', 'Games 3', 'Games 4', 'Games 5'];
+					
+					var firstAchievement = true;
+					
+					for(var i = 1; i < dataSucces.length; i++){
+					
+						if(dataSucces[i] == 1 && firstAchievement){
+							listeSucces = listeSucces + fieldsSucces[i];
+							firstAchievement = false;
+						}
+						if(dataSucces[i] == 1){
+							listeSucces = listeSucces + ', ' + fieldsSucces[i];
+						}
+					}
 				}
 			}
-		}
-		//requête pour chercher le joueur voulu
-		connection.query(qstr3 + "'" + nomChercher + "'", function(err, rows, fields) {
-			if (err) throw err;
-			if(rowsSucces[0] === undefined)
-        		console.log('user does not exist in database: ');
-        	else{
-				data = ['rows[0].avatar', rows[0].username, rows[0].nbJouer, rows[0].nbGagner, rows[0].ratioVictoire, rows[0].nbBallePerdue, rows[0].nbPoints, listeSucces];				
-				res.send(data);
-				}
+			pool.getConnection(function(err, connection5) {
+				//requête pour chercher le joueur voulu
+				connection5.query(qstr3 + "'" + nomChercher + "'", function(err, rows, fields) {
+					if (err) throw err;
+			
+					if(rows[0] === undefined){
+        				console.log('user does not exist in database');
+					}
+					else{
+						if(String(rows[0].username) != ''){
+							data = ['rows[0].avatar', rows[0].username, rows[0].nbJouer, rows[0].nbGagner, rows[0].ratioVictoire, rows[0].nbBallePerdue, rows[0].nbPoints, listeSucces];
+							console.log(listeSucces);				
+							res.send(data);
+						}
+					}
+					connection5.release();
+				});
+			});	
+			connection4.release();
 		});
 	});
 });
@@ -168,7 +218,7 @@ app.listen(5050, function() {
 // Execution
 //------------------------------------------------------------
 console.log('Executing query:');
-var qstr1 = 'SELECT username FROM users';
+var qstr1 = 'SELECT * FROM users';
 
 var qstr2 = "SELECT * FROM SUCCES WHERE username = ";
 
